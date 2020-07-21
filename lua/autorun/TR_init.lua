74fd3c3eb1a4f8a80499cdc884f0a21077f00c04
SupinePandora43 <supinepandoraggg@gmail.com>
2020-07-21 11:36:09 +0500

    1.0.1-beta

--- lua/autorun/TR_init.lua
+++ lua/autorun/TR_init.lua
@@ -1,8 +1,15 @@
+version_MAJOR = 1
+version_MINOR = 0
+version_PATCH = 1
+version_TAG = "beta"
+print(
+    (((((("Trailers Reborn " .. tostring(version_MAJOR)) .. ".") .. tostring(version_MINOR)) .. ".") .. tostring(version_PATCH)) .. "-") .. tostring(version_TAG)
+)
 AddCSLuaFile()
 if SERVER then
     include("TR/sv_init.lua")
     AddCSLuaFile("TR/cl_init.lua")
     include("TR/sh_deprecated.lua")
-elseif CLIENT then
+else
     include("TR/cl_init.lua")
 end
--- src/autorun/TR_init.ts
+++ src/autorun/TR_init.ts
@@ -1,8 +1,17 @@
+const version_MAJOR = 1
+const version_MINOR = 0
+const version_PATCH = 1
+const version_TAG = "beta"
+
+
+print("Trailers Reborn " + version_MAJOR + "." + version_MINOR + "." + version_PATCH + "-" + version_TAG)
+
+
 AddCSLuaFile()
 if (SERVER) {
 	include("TR/sv_init.lua")
 	AddCSLuaFile("TR/cl_init.lua")
 	include("TR/sh_deprecated.lua")
-} else if (CLIENT) {
+} else {
 	include("TR/cl_init.lua")
 }
